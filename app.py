2d35f4ceb31b5f5fb03905cbb0b749704b8cc614
Sam Blanton <sam.blanton@D4JH1J2KMF.local>
2023-08-25 13:09:49 -0500

    beta

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask_mail import Mail, Message
 from flask_apscheduler import APScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
+import psycopg2
 import csv
-
+import threading
+import os
 
 # Create a Flask app instance
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_username:your_password@localhost:5433/your_database_name'
 app.secret_key = 'fsdjfsldkfjskldjflksdjflksdjf' 
 db = SQLAlchemy(app)
 
@@ -22,14 +24,15 @@ app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
 
+time = datetime.now().strftime("%B %d, %Y %I:%M %p")
 
- 
 class Students(db.Model):
     studentID = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(120), nullable=False)
     lastname = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     borrowings = db.relationship('Borrower', backref='student', lazy=True)
+    card = db.relationship('Card', backref='student', lazy=True)
 
 class Borrower(db.Model):
     borrowerID = db.Column(db.Integer, primary_key=True)
@@ -37,32 +40,72 @@ class Borrower(db.Model):
     studentID = db.Column(db.Integer, db.ForeignKey('students.studentID'))
     turned_in = db.Column(db.Boolean(), nullable=False )
  
-class Historical(db.Model):
+class Card(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(120), nullable=False)
-    lastname = db.Column(db.String(120), nullable=False)
     what = db.Column(db.String(120), nullable=False)
+    studentID = db.Column(db.Integer, db.ForeignKey('students.studentID'))
 
+class Historical(db.Model): 
+    ID = db.Column(db.Integer, primary_key=True)
+    studentID = db.Column(db.Integer, db.ForeignKey('students.studentID'))
+    what = db.Column(db.String(120), nullable=False)
+    when = db.Column(db.DateTime, default=datetime.now())
+
+
+def write_to_log(message, category):
+    # Directory to store log files
+    log_dir = 'logs'
+    
+    # Create directory if it doesn't exist
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Define the path of the log file based on the category
+    log_file_path = os.path.join(log_dir, f"{category}.log")
+
+    # Write the message to the log file
+    with open(log_file_path, 'a') as file:
+        file.write(f"{message}\n")
 
 
 def send_charge_email():
     with app.app_context():
         # Fetch all borrowers
         borrowers = Borrower.query.all()
+        cards = Card.query.all()
         
-        if not borrowers:
+        if not borrowers and cards:
+            print('Nothing to send')
             return
         
         summary_list = []
-        for borrower in borrowers:
-            student = Students.query.filter_by(studentID=borrower.studentID).first()
+        if borrowers:
+            for borrower in borrowers:
+                student = Students.query.filter_by(studentID=borrower.studentID).first()
 
-            borrowed_item = borrower.what
-            charge = "$5" if borrowed_item == "Charger" else "$10" if borrowed_item == "Computer" else "$0"
+                borrowed_item = borrower.what
+                charge = "$5" if borrowed_item == "Charger" else "$10" if borrowed_item == "Computer" else "$0"
 
-            summary_list.append(f"{student.firstname} {student.lastname} - {borrowed_item}: {charge}")
+                summary_list.append(f"{student.firstname} {student.lastname} - {borrowed_item}: {charge}")
+
+        if cards:
+            for card in cards:
+                student = Students.query.filter_by(studentID=card.studentID).first()
+
+                replace_id = card.what
+                charge = "$10"
+
+                summary_list.append(f"{student.firstname} {student.lastname} - {replace_id}: {charge}")
+
+                db.session.delete(card)
+
+            try:
+                db.session.commit()
+            except Exception as e:
+                write_to_log(f"{time}-{str(e)}", "error")
+                db.session.rollback()
         
-        # Convert the list to a string
+        
         summary_str = "\n".join(summary_list)
 
         # Craft the email message
@@ -72,15 +115,16 @@ def send_charge_email():
         
         msg.body = f"***This is an automated email***\n\n{summary_str}"
         
-        # Send the email
-        mail.send(msg)
+        try:
+            mail.send(msg)
+        except Exception as e:
+            write_to_log(f"{time}-{str(e)}", "error")
+
         
-        # Craft the email message
-        # List of recipients where turned_in is True
         recipients_turned_in = [borrower.student.email for borrower in borrowers if borrower.turned_in]
 
-        # List of recipients where turned_in is False
         recipients_not_turned_in = [borrower.student.email for borrower in borrowers if not borrower.turned_in]
+
 
         if recipients_turned_in:
             msg1 = Message("Borrowed Item was turned in",
@@ -98,7 +142,8 @@ def send_charge_email():
 
             try:
                 db.session.commit()
-            except:
+            except Exception as e:
+                write_to_log(f"{time}-{str(e)}", "error")
                 db.session.rollback()
 
         if recipients_not_turned_in:
@@ -106,31 +151,45 @@ def send_charge_email():
                         sender="sblanton@mrapats.com",
                         bcc=recipients_not_turned_in)
             
-            msg2.body = f"***This is an automated email***\n\nYour borrowed item was not turned in. A charge has been added to your MyBackPack. If the device is not turned in by tomorrow 5pm you will recieve another charge."
-            mail.send(msg2)
+            msg2.body = f"***This is an automated email***\n\nYour borrowed item was not turned in. A charge has been added to your MyBackPack. Please turn in your item to stop future charges" 
+            
+            try:
+                mail.send(msg2) 
+            except Exception as e:
+                write_to_log(f"{time}-{str(e)}", "error")
+
+
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=send_charge_email, trigger="interval", days=1, start_date='2023-08-22 14:26:00') # assuming you want it daily at 5pm
+scheduler.add_job(func=send_charge_email, trigger="interval", days=1, start_date='2023-08-22 08:58:00') # assuming you want it daily at 5pm 
 #scheduler.add_job(func=send_charge_email, trigger="interval", days=1, start_date='2023-08-22 17:00:00') # assuming you want it daily at 5pm
 scheduler.start()
         
 
 def send_confirmation_email(studentID):
     with app.app_context():
+        try:
+            borrower = Borrower.query.filter_by(studentID=studentID).first()
 
-        borrower = Borrower.query.filter_by(studentID=studentID).first()
+            # Craft the email message
+            recipients = [borrower.student.email]
+            
+            msg = Message(f"{borrower.what} checkout confirmation",
+                        sender="sblanton@mrapats.com",
+                        recipients=recipients)
+            
+            msg.body = f"***This is an automated email***\n\nYou have borrowed a {str.lower(borrower.what)} and a your my backpack account will be charged at 5pm. If the {str.lower(borrower.what)} is not returned befor 5pm you will be charged for another day."
+            
+            # Send the email
+            mail.send(msg)
+        except Exception as e:
+            # Log the error message
+            write_to_log(f"{time}-{str(e)}", "error")
 
-        # Craft the email message
-        recipients = [borrower.student.email]
-        
-        msg = Message(f"{borrower.what} checkout confirmation",
-                      sender="sblanton@mrapats.com",
-                      recipients=recipients)
-        
-        msg.body = f"***This is an automated email***\n\nYou have borrowed a {str.lower(borrower.what)} and a your my backpack account will be charged at 5pm."
-        
-        # Send the email
-        mail.send(msg)
+
+def send_email_in_background(studentID):
+    email_thread = threading.Thread(target=send_confirmation_email, args=(studentID,))
+    email_thread.start()
 
 
 def load_students_from_csv(file_path):
@@ -161,7 +220,8 @@ def load_students_from_csv(file_path):
             try:
                 db.session.commit()
                 flash('Students loaded successfully!', 'success')
-            except:
+            except Exception as e:
+                write_to_log(f"{time}-{str(e)}", "error")
                 db.session.rollback()
                 flash('Error occurred while loading students.', 'danger')
 
@@ -177,6 +237,11 @@ def startup():
 def hello_world():
  
     return render_template('index.html') 
+
+@app.route('/cards')
+def show_cards():
+    all_cards = Card.query.all()
+    return render_template('cards.html', cards=all_cards)
 
  
 @app.route('/add_borrower', methods=['POST'])
@@ -200,17 +265,39 @@ def add_borrower():
 
         # If student exists
         new_borrower = Borrower(what=what, studentID=student.studentID, turned_in=False)
-
+        historical = Historical(what=what, studentID=student.studentID)
+        
         try:
             db.session.add(new_borrower)
+            db.session.add(historical)
             db.session.commit()
-            send_confirmation_email(student.studentID)
+            send_email_in_background(student.studentID)
             flash('Item borrowed successfully', 'success')
             return redirect('/')
-        except:
+        except Exception as e:
+            write_to_log(f"{time}-{str(e)}", "error")
             flash('Error occurred. Please try again.', 'danger')
             return redirect('/')
     
+
+@app.route('/borrower', methods=['POST','GET'])
+def borrower():
+    borrowers = Borrower.query.all()
+    if request.method == 'POST':
+        borrowerID = request.form['borrowerID'] 
+        print(borrowerID)
+        borrower = db.session.get(Borrower, borrowerID)
+        db.session.delete(borrower)
+        try:
+            db.session.commit()
+            flash('Student absolved', 'success')
+            return redirect('/borrower')
+        except Exception as e:
+            write_to_log(f"{time}-{str(e)}", "error")
+            flash('Error occurred. Please try again.', 'danger')
+            return redirect('/borrower')
+    
+    return render_template('borrower.html', borrowers=borrowers)
 
 @app.route('/return', methods=['POST'])
 def returning():
@@ -244,10 +331,62 @@ def returning():
             return redirect('/')
             
         except Exception as e:
+            write_to_log(f"{time}-{str(e)}", "error")
             flash(f"Error updating student email: {e}", 'danger')
             return redirect('/')
 
+@app.route('/replace_id', methods=['POST'])
+def replace_id():
 
+    if request.method == 'POST':
+        email = request.form['email']
+        student = Students.query.filter_by(email=email).first()
+
+        
+
+        if not student:
+            flash('Student not found. Please ensure you are registered.', 'danger')
+            return redirect('/')
+
+        card_entry = Card.query.filter_by(studentID=student.studentID).first()
+
+        if card_entry:
+            flash('You have already ordered an ID today', 'danger')
+            return redirect('/')
+        
+        new_card = Card(what="ID", studentID=student.studentID)
+        historical = Historical(what="ID", studentID=student.studentID)
+        db.session.add(historical)
+        db.session.add(new_card)
+
+        try:
+            db.session.commit()
+            flash("Your ID has been ordered. Come pick it up later today.", 'success')
+            return redirect('/')
+            
+        except Exception as e:
+            write_to_log(f"{time}-{str(e)}", "error")
+            flash(f"Error ordering ID: {e}", 'danger')
+            return redirect('/')
+        
+@app.route('/historical', methods=['GET', 'POST'])
+def historical(): 
+    
+    if request.method == 'POST':
+        search = request.form.get('search')
+        students = Students.query.filter(Students.lastname.ilike(f"%{search}%")).all()
+        return render_template('history.html', students=students)
+    else:
+        students = Students.query.all()  # Fetch all Students records
+        return render_template('history.html', active_page='Students', students=students)
+    
+@app.route('/student/<int:studentID>/', methods=['GET'])
+def student_details(studentID):
+    student = Students.query.get_or_404(studentID)
+    history = Historical.query.filter_by(studentID=studentID).all()
+    
+
+    return render_template('student_details.html', student=student, history=history)
 
 
 # Run the app when this script is executed
